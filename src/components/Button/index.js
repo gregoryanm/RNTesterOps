5218d2cbd563f9d6df37b2bca099d63795033394
Mark Gregory <markgregory.scp@gmail.com>
2018-11-20 04:02:57 -0600

    Buttons

---
+++ src/components/Button/index.js
@@ -0,0 +1,76 @@
+/**
+ * Created by InspireUI on 17/02/2017.
+ *
+ * @format
+ */
+
+import React, { PureComponent } from "react";
+import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
+import Color from "../../constants/Colors";
+import { Icon } from "../../constants/Icons";
+
+class Button extends PureComponent {
+  // constructor(props) {
+  //   super(props)
+  // }
+
+  render() {
+    const {
+      text,
+      icon,
+      onPress,
+      button,
+      containerStyle,
+      textStyle,
+      containerColor,
+      textColor
+    } = this.props;
+    return (
+      <TouchableOpacity
+        style={[
+          styles.container,
+          button,
+          { backgroundColor: containerColor },
+          containerStyle
+        ]}
+        onPress={onPress}
+      >
+        {icon ? (
+          <Icon name={icon} color={textColor} size={24} style={styles.icon} />
+        ) : (
+          <View />
+        )}
+        <Text style={[styles.text, { color: textColor }, textStyle]}>
+          {text}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: 44,
+    padding: 10,
+    flexDirection: "row"
+  },
+  text: {
+    fontWeight: "bold"
+  },
+  icon: {
+    marginRight: 10
+  }
+});
+
+Button.defaultProps = {
+  text: "Button",
+  onPress: () => "Button pressed!",
+  containerStyle: {},
+  textStyle: {},
+  containerColor: Color.theme2,
+  textColor: "white"
+};
+
+export default Button;
